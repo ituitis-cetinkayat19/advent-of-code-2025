f8cc302d729e4549fdeb9fc9dee9466e445f1b2a
Tarık Çetinkaya <tarikc134@gmail.com>
2026-07-06 20:48:47 +0300

    long instead of string

--- day2/part1/main.cpp
+++ day2/part1/main.cpp
@@ -19,15 +19,15 @@ n basamaklı -> 9 * 10 ^ (n/2 - 1)
 199199
 */
 
-vector<pair<string, string>> splitInput(const string& lineText) {
-  vector<pair<string, string>> vector;
+vector<pair<long, long>> splitInput(const string& lineText) {
+  vector<pair<long, long>> vector;
   stringstream ss(lineText);
   string pairText;
   while (getline(ss, pairText, ',')) {
-    pair<string, string> pair;
+    pair<long, long> pair;
     int splitIndex = pairText.find('-');
-    pair.first = pairText.substr(0, splitIndex);
-    pair.second = pairText.substr(splitIndex + 1);
+    pair.first = stol(pairText.substr(0, splitIndex));
+    pair.second = stol(pairText.substr(splitIndex + 1));
     vector.push_back(pair);
   }
   return vector;
@@ -37,6 +37,6 @@ int main() {
   string lineText;
   ifstream InputFile("test-input.txt");
   getline(InputFile, lineText);
-  vector<pair<string, string>> ranges = splitInput(lineText);
+  vector<pair<long, long>> ranges = splitInput(lineText);
   return 0;
 }
