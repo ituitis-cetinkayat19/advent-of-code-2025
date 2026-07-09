95ef59468955619fc0c24eb053e02298d67aa4c2
Tarık Çetinkaya <tarikc134@gmail.com>
2026-07-09 22:16:24 +0300

    day 5 solved

---
+++ day5/part1/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <sstream>
+using namespace std;
+
+bool isFresh(const vector<pair<long long, long long>>& ranges, const long long& id) {
+  for (const pair<long long, long long>& range : ranges) {
+    long long first = range.first;
+    long long second = range.second;
+    if (id >= first && id <= second) return true;
+  }
+  return false;
+}
+
+int main() {
+  string lineText;
+  ifstream InputFile("input.txt");
+  vector<pair<long long, long long>> freshRanges;
+  vector<long long> idsToCheck;
+  while (getline(InputFile, lineText) && !lineText.empty()) {
+    pair<long long, long long> range;
+    int splitIndex = lineText.find('-');
+    long long first = stoll(lineText.substr(0, splitIndex));
+    long long second = stoll(lineText.substr(splitIndex + 1));
+    range.first = first;
+    range.second = second;
+    freshRanges.push_back(range);
+  }
+  while (getline(InputFile, lineText)) {
+    idsToCheck.push_back(stoll(lineText));
+  }
+  int total = 0;
+  for (long long& id : idsToCheck) {
+    if (isFresh(freshRanges, id)) total++;
+  }
+  cout << "Total: " << total;
+  return 0;
+}
